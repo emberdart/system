691649ce2ee8f25b012b894c0230d5f3704c038b
Ember Dart <git@emberdart.co.uk>
2026-07-04 21:12:05 +0100

    silly fetchers

--- common/packages/console/fun.nix
+++ common/packages/console/fun.nix
@@ -1,11 +1,15 @@
 pkgs:
 with pkgs; [
     ddate
+    fastfetch
     fortune
     genact
     # haxor-news # ERROR Backend 'setuptools.build_meta:__legacy__' is not available. - python3.13-click-7.1.2.drv' failed with exit code 1
     hollywood
+    # hyfetch # doesn't build properly?
     lavat
+    # neofetch # use neowofetch from hyfetch
+    nitch
     nms
     screenfetch
 ]
