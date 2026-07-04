b882681cf0862faa1be2623a2a43877c78de14d0
Ember Dart <git@emberdart.co.uk>
2026-07-04 21:11:40 +0100

    use 26.05

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
-sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-26.05 nixos
 # sudo nix-store --verify --check-contents --repair
 # sudo rm -rf /boot/*
 "$HERE"/update.sh && \
