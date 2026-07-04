ad5d04bea3bddcb1f39cd80eebcedbd1a5e7202d
Ember Dart <git@emberdart.co.uk>
2026-07-04 21:12:25 +0100

    stop my own for now

--- common/packages/console/mine.nix
+++ common/packages/console/mine.nix
@@ -1,10 +1,10 @@
-pkgs:
-let haskellRepo = /home/dwd/code/mine/haskell;
-    chatio = import "${haskellRepo}/chatio/default.nix" {};
-    dubloons = import "${haskellRepo}/dubloons/default.nix" {};
-    jobfinder-server = import "${haskellRepo}/jobfinder/api.nix" {};
-in [
-    chatio
-    dubloons
-    jobfinder-server
-]
+# pkgs:
+# let haskellRepo = ${homeDirectory}/code/mine/haskell;
+#     chatio = import "${haskellRepo}/chatio/default.nix" {};
+#     dubloons = import "${haskellRepo}/dubloons/default.nix" {};
+#     jobfinder-server = import "${haskellRepo}/jobfinder/api.nix" {};
+# in [
+#     chatio
+#     dubloons
+#     jobfinder-server
+# ]
